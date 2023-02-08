90216824b788f99ab9cb96c4386da935e2869068
anita <anubhui27@gmail.com>
2023-02-08 23:03:38 +0530

    autowiring types

---
+++ src/main/java/com/spring/auto/Test.java
@@ -0,0 +1,21 @@
+package com.spring.auto;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Resource res = new ClassPathResource("spring.xml");
+		BeanFactory factory = new XmlBeanFactory(res);
+
+		Categories categories = (Categories) factory.getBean("categories");
+		categories.show();
+
+
+
+	}
+
+}
